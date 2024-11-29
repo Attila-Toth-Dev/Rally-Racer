17eadf1c15d1fd8e813ad89fb92ebdc453d89719
Attila Toth <attilatwork@gmail.com>
2024-11-30 09:07:29 +1100

    ADDED: added a check for sides of wheels for raycasthits

--- Rally-Racer-V2/Assets/Scripts/Collisions/RayWheelCollision.cs
+++ Rally-Racer-V2/Assets/Scripts/Collisions/RayWheelCollision.cs
@@ -10,6 +10,7 @@ public class RayWheelCollision : MonoBehaviour
     [Header("Raycast Settings")]
     [SerializeField] private int raysNumber = 15;
     [SerializeField] private float raysMaxAngle = 180f;
+    [SerializeField] private float wheelWidth = .4f;
     
     private WheelCollider _wheelCollider;
     private float _orgRadius;
@@ -20,33 +21,72 @@ public class RayWheelCollision : MonoBehaviour
         _orgRadius = _wheelCollider.radius;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         float radiusOffset = 0f;
         
-        for (int i = 0; i <= raysNumber; ++i)
+        CreateRaycastCheck(radiusOffset);
+        CreateRaycastRightCheck(radiusOffset);
+        CreateRaycastLeftCheck(radiusOffset);
+    }
+
+    private void CreateRaycastCheck(float _radiusOffset)
+    {
+        for (int i = 0; i <= raysNumber; i++)
         {
-            Vector3 rayDirection = Quaternion.AngleAxis(_wheelCollider.steerAngle, transform.up) * Quaternion.AngleAxis(i * (raysMaxAngle / raysNumber) + (180f - raysMaxAngle) / 2f, transform.right) * transform.forward;
-            
-            // Handles the radius of the wheel collider with spherecast
-            if (Physics.SphereCast(wheelObject.position, .5f, rayDirection, out RaycastHit sphereCastHit, _wheelCollider.radius))
-            {
-                Debug.DrawRay(wheelObject.position, sphereCastHit.point, Color.red);
-                
-                radiusOffset = Mathf.Max(radiusOffset, _wheelCollider.radius - sphereCastHit.distance);
-            }
+            Vector3 rayDirection = Quaternion.AngleAxis(_wheelCollider.steerAngle, transform.up) * Quaternion.AngleAxis(i * (raysMaxAngle / raysNumber) + ((180f - raysMaxAngle) / 2f), transform.right) * transform.forward;
             
             // Handles the radius of the wheel collider with raycast
-            //if(Physics.Raycast(wheelObject.position, rayDirection, out RaycastHit raycastHit, _wheelCollider.radius))
-            //{
-            //    Debug.DrawRay(wheelObject.position, raycastHit.point, Color.red);
-            //    
-            //    radiusOffset = Mathf.Max(radiusOffset, _wheelCollider.radius - raycastHit.distance);
-            //}
+            if(Physics.Raycast(wheelObject.position, rayDirection, out RaycastHit raycastHit, _wheelCollider.radius)  && !raycastHit.collider.isTrigger)
+            {
+                Debug.DrawLine(wheelObject.position, raycastHit.point, Color.red);
+                
+                _radiusOffset = Mathf.Max(_radiusOffset, _wheelCollider.radius - raycastHit.distance);
+            }
             
             Debug.DrawRay(wheelObject.position, rayDirection * _orgRadius, Color.green);
         }
         
-        _wheelCollider.radius = Mathf.LerpUnclamped(_wheelCollider.radius, _orgRadius + radiusOffset, Time.deltaTime * 10f);
+        _wheelCollider.radius = Mathf.LerpUnclamped(_wheelCollider.radius, _orgRadius + _radiusOffset, Time.deltaTime * 5f);
+    }
+
+    private void CreateRaycastRightCheck(float _radiusOffset)
+    {
+        for (int i = 0; i <= raysNumber; i++)
+        {
+            Vector3 rayDirection = Quaternion.AngleAxis(_wheelCollider.steerAngle, transform.up) * Quaternion.AngleAxis(i * (raysMaxAngle / raysNumber) + ((180f - raysMaxAngle) / 2f), transform.right) * transform.forward;
+            
+            // Handles the radius of the wheel collider with raycast
+            if(Physics.Raycast(wheelObject.position + wheelObject.right * wheelWidth * .5f, rayDirection, out RaycastHit raycastHit, _wheelCollider.radius)  && !raycastHit.collider.isTrigger)
+            {
+                Debug.DrawLine(wheelObject.position + wheelObject.right * wheelWidth * .5f, raycastHit.point, Color.red);
+                
+                _radiusOffset = Mathf.Max(_radiusOffset, _wheelCollider.radius - raycastHit.distance);
+            }
+            
+            Debug.DrawRay(wheelObject.position + wheelObject.right * wheelWidth * .5f, rayDirection * _orgRadius, Color.green);
+        }
+        
+        _wheelCollider.radius = Mathf.LerpUnclamped(_wheelCollider.radius, _orgRadius + _radiusOffset, Time.deltaTime * 5f);
+    }
+    
+    private void CreateRaycastLeftCheck(float _radiusOffset)
+    {
+        for (int i = 0; i <= raysNumber; i++)
+        {
+            Vector3 rayDirection = Quaternion.AngleAxis(_wheelCollider.steerAngle, transform.up) * Quaternion.AngleAxis(i * (raysMaxAngle / raysNumber) + ((180f - raysMaxAngle) / 2f), transform.right) * transform.forward;
+            
+            // Handles the radius of the wheel collider with raycast
+            if(Physics.Raycast(wheelObject.position - wheelObject.right * wheelWidth * .5f, rayDirection, out RaycastHit raycastHit, _wheelCollider.radius)  && !raycastHit.collider.isTrigger)
+            {
+                Debug.DrawLine(wheelObject.position - wheelObject.right * wheelWidth * .5f, raycastHit.point, Color.red);
+                
+                _radiusOffset = Mathf.Max(_radiusOffset, _wheelCollider.radius - raycastHit.distance);
+            }
+            
+            Debug.DrawRay(wheelObject.position - wheelObject.right * wheelWidth * .5f, rayDirection * _orgRadius, Color.green);
+        }
+        
+        _wheelCollider.radius = Mathf.LerpUnclamped(_wheelCollider.radius, _orgRadius + _radiusOffset, Time.deltaTime * 10f);
     }
 }
